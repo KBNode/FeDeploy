79028cfe4a72255e27d7a1a3e57970db0c1a676d
zhangshine <zhangshine0125@gmail.com>
2018-01-02 13:29:03 +0800

    new logger

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 const spawn = require('child_process').spawn;
 const fs = require('fs');
+const split2 = require('split2');
+const through2 = require('through2');
 const express = require('express');
 const app = express();
 const argv = require('minimist')(process.argv.slice(2));
 
 let config = JSON.parse(fs.readFileSync(argv.config));
-let logStream = fs.createWriteStream(config.log);
+let logStream = fs.createWriteStream(config.log, {flags: 'a'});
 
+function prefixStream (stream, prefix) {
+    return stream.pipe(split2()).pipe(through2(function (data, enc, callback) {
+        callback(null, prefix + data + '\n')
+    }))
+}
 
-function run_cmd(cmd, args, callback) {
+function run_cmd(cmd, args) {
     var spawn = require('child_process').spawn;
     var child = spawn(cmd, args, {env: process.env});
-    var resp = "";
 
-    child.stdout.on('data', function(buffer) {
-        resp += buffer.toString();
-    });
-    child.stdout.on('end', function() {
-        callback (resp)
-    });
-    child.on('error', function (err) {
-        console.log(err.message);
-    });
     child.on('close', function (code) {
         console.log(code);
-    })
+    });
+
+    prefixStream(child.stdout, '[' + new Date() + ']stdout: ').pipe(logStream, { end: false });
+    prefixStream(child.stderr, '[' + new Date() + ']stderr: ').pipe(logStream, { end: false });
 }
 
 app.post('/web-hook', (req, res) => {
     let project = req.query.project,
         projectExec = config.projects[project].exec;
-    run_cmd('sh', ['-c', projectExec], (text)=>{
-        logStream.write(new Date() + '\n');
-        logStream.write(text + '\n');
-    });
+    projectExec = Array.isArray(projectExec) ? projectExec : [ 'sh', '-c', projectExec ];
+    run_cmd(projectExec.shift(), projectExec);
     res.send('success');
 });
 
